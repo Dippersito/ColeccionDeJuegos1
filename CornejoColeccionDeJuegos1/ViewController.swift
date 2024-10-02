7e36127074fb1b7467dde6d28f7d2e1e355fc0b5
Dippersito <stephano.cornejo@tecsup.edu.pe>
2024-10-02 18:43:43 -0500

    Initial Commit

---
+++ CornejoColeccionDeJuegos1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CornejoColeccionDeJuegos1
+//
+//  Created by Stephano on 2/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
